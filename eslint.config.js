5e3347834f3992eb0da77e4b522fb41abeb752d0
D5ng <121039744+D5ng@users.noreply.github.com>
2026-02-17 03:33:25 +0900

    style: 프로젝트 내 코드 컨벤션 수립 (#4)
    * chore: vscode 서식 지정 및 린팅 설정을 추가합니다.
    
    * chore: .gitignore에서 .vscode 디렉토리 제외 항목 제거
    
    * chore: code formatting을 위해 .prettierrc 파일 추가
    
    * chore: .prettierignore 파일 추가하여 포맷팅 제외 항목 설정
    
    * chore: 코드 품질 향상을 위해 ESLint 플러그인 및 설정을 추가
    
    * chore: ESLint 설정을 업데이트하고 추가 규칙 및 플러그인을 통합하여 코드 품질을 향상
    
    - eslint import 추가
    - eslint react 추가
    - eslint a11y 추가
    - eslint prettier 추가
    
    * chore: 설정 파일의 일관성과 명확성을 위해 import 문을 리팩토링
    
    * docs: 코드 컨벤션 문서 추가 (ESLint + Prettier)로 포맷팅 및 규칙 정리
    
    * docs: md 파일 포멧팅

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,111 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import tseslint from "typescript-eslint"
+import { defineConfig, globalIgnores } from "eslint/config"
+import importPlugin from "eslint-plugin-import"
+import reactPlugin from "eslint-plugin-react"
+import eslintConfigPrettier from "eslint-config-prettier/flat"
+import jsxA11y from "eslint-plugin-jsx-a11y"
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
+      importPlugin.flatConfigs.recommended,
+      reactPlugin.configs.flat.recommended,
+      reactPlugin.configs.flat["jsx-runtime"],
+      eslintConfigPrettier,
+      jsxA11y.flatConfigs.recommended,
     ],
     languageOptions: {
+      ...reactPlugin.configs.flat.recommended.languageOptions,
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.serviceworker,
+        ...globals.browser,
+      },
+      parserOptions: {
+        projectService: true,
+      },
+    },
+    settings: {
+      "import/resolver": {
+        typescript: {
+          project: ["./tsconfig.json"],
+        },
+        node: {
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+          moduleDirectory: ["node_modules", "src"],
+        },
+      },
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "no-unused-vars": "off",
+      "import/no-dynamic-require": "warn",
+      "import/no-nodejs-modules": "warn",
+      "import/consistent-type-specifier-style": ["error", "prefer-top-level"],
+      "import/no-cycle": "warn",
+
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/await-thenable": "error",
+      "@typescript-eslint/consistent-type-imports": [
+        "error",
+        { prefer: "type-imports", fixStyle: "separate-type-imports" },
+      ],
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", "type", "index", "unknown"],
+          pathGroups: [
+            { pattern: "react", group: "external", position: "before" },
+            { pattern: "@/**", group: "internal" },
+          ],
+          "newlines-between": "always",
+          alphabetize: { order: "asc", caseInsensitive: true },
+        },
+      ],
+      "import/no-duplicates": "error",
+      "import/no-default-export": "error",
+      "react/function-component-definition": [
+        "error",
+        {
+          namedComponents: "function-declaration",
+          unnamedComponents: "arrow-function",
+        },
+      ],
+      "react/jsx-no-leaked-render": ["error", { validStrategies: ["coerce", "ternary"] }],
+      "react/jsx-pascal-case": ["error", { allowAllCaps: false }],
+      "react/no-array-index-key": "error",
+      "react/jsx-no-useless-fragment": "error",
+      "react/jsx-no-bind": [
+        "warn",
+        {
+          ignoreDOMComponents: true,
+          allowArrowFunctions: true,
+          allowFunctions: false,
+          allowBind: false,
+        },
+      ],
+      "jsx-a11y/alt-text": "error",
+      "jsx-a11y/click-events-have-key-events": "warn",
+      "jsx-a11y/no-static-element-interactions": "warn",
+      "jsx-a11y/anchor-is-valid": "error",
+    },
+  },
+  {
+    files: ["**/*.tsx"],
+    rules: {
+      "import/no-default-export": "off",
     },
   },
 ])
