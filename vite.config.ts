9f21becd249593c644e709e123024ea3975d9796
d5ng <dong5ffice@gmail.com>
2026-02-17 15:27:29 +0900

    chore: vite.config.ts에 tailwindcss 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
@@ -5,6 +6,7 @@ import { defineConfig } from "vite"
 // eslint-disable-next-line import/no-default-export
 export default defineConfig({
   plugins: [
+    tailwindcss(),
     react({
       babel: {
         plugins: [["babel-plugin-react-compiler"]],
