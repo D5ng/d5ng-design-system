605cec3efc107fead3271c77698ea21e5a87a8ad
d5ng <dong5ffice@gmail.com>
2026-02-23 01:56:08 +0900

    feat: createContext 유틸리티 함수 추가하여 React Context 생성 및 사용 간소화

---
+++ src/shared/utils/create-context.tsx
@@ -0,0 +1,32 @@
+import React from "react"
+
+export function createContext<ContextValuesType extends object>({
+  contextName,
+  defaultContextValues,
+}: {
+  contextName: string
+  defaultContextValues?: ContextValuesType
+}) {
+  const Context = React.createContext<ContextValuesType | undefined>(defaultContextValues ?? undefined)
+
+  function Provider({ children, ...contextValues }: ContextValuesType & { children: React.ReactNode }) {
+    const value = React.useMemo(
+      () => contextValues,
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+      Object.values(contextValues),
+    ) as ContextValuesType
+
+    return <Context.Provider value={value}>{children}</Context.Provider>
+  }
+
+  function useContext() {
+    const context = React.useContext(Context)
+
+    if (context) return context
+    if (defaultContextValues !== undefined) return defaultContextValues
+
+    throw new Error(`${contextName}Context는 ${contextName}Provider 내부에서만 사용할 수 있어요.`)
+  }
+
+  return [Provider, useContext] as const
+}
