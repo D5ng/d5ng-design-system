97e17b115fecc50487e4f6f39a75119fbb83071c
D5ng <121039744+D5ng@users.noreply.github.com>
2026-03-01 01:53:46 +0900

    enhancement: Slot 컴포넌트에 ref를 등록할 수 있는 기능 개선 및 테스트 코드 작성 (#20)
    * feat: slottable 컴포넌트 구현
    
    - Slot 컴포넌트로 렌더링 될 컴포넌트의 children에 들어갈 수 있는 기능
    
    * feat: slot 컴포넌트 추가
    
    - slot 컴포넌트를 구현하여 Slottable 컴포넌트의 children을 처리하는 기능 추가
    - 다중 children 사용 시 경고 메시지 출력
    
    * feat: 상위 컴포넌트의 ref를 하위 컴포넌트에서 사용할 수 있도록 코드 변경
    
    * test: slot 컴포넌트에 대한 unit test 추가
    
    * feat: composeRefs 및 setRef 유틸리티 함수 추가
    
    - 여러 개의 ref를 결합하고 동기화할 수 있는 composeRefs 함수 구현
    - Callback 및 Object 형태의 ref를 처리하는 setRef 함수 추가
    
    * feat: slot 컴포넌트에서 ref 결합 로직 개선
    
    - Slot 컴포넌트의 ref 처리 방식을 composeRefs 함수를 사용하여 개선
    - getElementRef 함수를 추가하여 자식 요소의 ref를 가져오는 기능 구현 (React 19)
    
    * test: slot 컴포넌트의 ref 병합 동작에 대한 단위 테스트 추가
    
    * docs: getElementRef  유틸리티 함수에 jsdoc 작성
    
    * docs: slot 컴포넌트 jsdoc 작성

---
+++ src/utils/slot.spec.tsx
@@ -0,0 +1,140 @@
+import type { ButtonHTMLAttributes, ReactNode } from "react"
+
+import { fireEvent, render, screen } from "@testing-library/react"
+import { createRef } from "react"
+import { describe, it } from "vitest"
+
+import { Slot } from "./slot"
+import { Slottable } from "./slottable"
+
+describe("Slot 컴포넌트 상세 검증", () => {
+  describe("렌더링 제약 사항", () => {
+    it("자식 요소가 하나라면, 그대로 렌더링 되어야 한다", () => {
+      render(
+        <Slot>
+          <p data-testid="slot-text">Slot</p>
+        </Slot>,
+      )
+      expect(screen.getByTestId("slot-text")).toBeInTheDocument()
+    })
+
+    it("자식 요소가 여러 개라면, 에러를 발생시킨다", () => {
+      const consoleSpy = vi.spyOn(console, "error").mockImplementation(() => {})
+
+      expect(() => {
+        render(
+          <Button asChild>
+            <p data-testid="slot-text">Slot</p>
+            <p data-testid="slot-text">Slot</p>
+          </Button>,
+        )
+      }).toThrow()
+
+      consoleSpy.mockRestore()
+    })
+  })
+
+  describe("속성 위임 및 병합", () => {
+    it("Slot 컴포넌트에 전달된 속성이 올바르게 하위 컴포넌트에 전달되어야 한다.", () => {
+      render(
+        <Slot className="parent-class" id="parent-id">
+          <button className="child-class">버튼</button>
+        </Slot>,
+      )
+
+      const button = screen.getByRole("button")
+
+      expect(button).toHaveClass("parent-class")
+      expect(button).toHaveAttribute("id", "parent-id")
+    })
+
+    it("이벤트 핸들러가 하위 컴포넌트로 전달되어 동작해야 한다.", () => {
+      const handleClick = vi.fn()
+
+      render(
+        <Button asChild onClick={handleClick}>
+          <button>버튼</button>
+        </Button>,
+      )
+
+      const button = screen.getByRole("button")
+      fireEvent.click(button)
+      expect(handleClick).toHaveBeenCalledTimes(1)
+    })
+
+    it("Ref가 자식의 실제 DOM 요소를 올바르게 가리켜야 한다", () => {
+      const ref = createRef<HTMLButtonElement>()
+      render(
+        <Slot ref={ref}>
+          <button>ref 확인</button>
+        </Slot>,
+      )
+
+      expect(ref.current).toBeInstanceOf(HTMLButtonElement)
+      expect(ref.current?.textContent).toBe("ref 확인")
+    })
+
+    it("상위의 forwardRef와 하위의 ref가 모두 동일한 DOM 요소를 가리켜야 한다.", () => {
+      const parentRef = createRef<HTMLButtonElement>()
+      const childRef = createRef<HTMLButtonElement>()
+
+      render(
+        <Slot ref={parentRef}>
+          <button ref={childRef} data-testid="target-button">
+            ref 병합
+          </button>
+        </Slot>,
+      )
+
+      const renderedButton = screen.getByTestId("target-button")
+
+      expect(parentRef.current).toBe(renderedButton)
+      expect(childRef.current).toBe(renderedButton)
+      expect(parentRef.current).toBe(childRef.current)
+    })
+  })
+
+  describe("Slottable 컴포넌트 렌더링 테스트", () => {
+    it("Slottable을 사용해 아이콘과 동적 자식이 올바르게 조립되어야 한다", () => {
+      render(
+        <Slot>
+          <span data-testid="left-icon">Icon</span>
+          <Slottable>
+            <button data-testid="main-button">Button</button>
+          </Slottable>
+          <span data-testid="right-icon">Icon</span>
+        </Slot>,
+      )
+
+      const button = screen.getByTestId("main-button")
+      const leftIcon = screen.getByTestId("left-icon")
+      const rightIcon = screen.getByTestId("right-icon")
+
+      expect(button).toBeInTheDocument()
+      expect(leftIcon).toBeInTheDocument()
+      expect(rightIcon).toBeInTheDocument()
+    })
+  })
+})
+
+function Button({
+  children,
+  asChild,
+  leftIcon,
+  rightIcon,
+  ...restProps
+}: {
+  asChild: boolean
+  leftIcon?: ReactNode
+  rightIcon?: ReactNode
+} & ButtonHTMLAttributes<HTMLButtonElement>) {
+  const Component = asChild ? Slot : "button"
+
+  return (
+    <Component {...restProps}>
+      {leftIcon}
+      <Slottable>{children}</Slottable>
+      {rightIcon}
+    </Component>
+  )
+}
--- src/utils/slot.tsx
+++ src/utils/slot.tsx
@@ -1,14 +1,26 @@
-import type { HTMLAttributes, ReactElement, ReactNode } from "react"
+import type { HTMLAttributes, ReactElement, ReactNode, Ref } from "react"
 
-import { Children, cloneElement, isValidElement } from "react"
+import { Children, cloneElement, forwardRef, isValidElement } from "react"
 
+import { composeRefs } from "./compose-refs"
 import { Slottable } from "./slottable"
+
+type MergePropsWithRef<P> = P & { ref?: Ref<HTMLElement> }
 
 interface SlotProps extends HTMLAttributes<HTMLElement> {
   children: ReactNode
 }
 
-export function Slot({ children, ...restProps }: SlotProps) {
+/**
+ * 상위 컴포넌트가 하위 컴포넌트에게 렌더링을 위임하는 유틸리티 컴포넌트
+ * @param children - 하위 컴포넌트
+ * @param restProps - 나머지 속성
+ * @param forwardedRef - 상위 컴포넌트에서 전달된 ref
+ * @returns ReactElement
+ *
+ * @see {@link https://github.com/radix-ui/primitives/blob/main/packages/react/slot/src/slot.tsx Radix UI의 Slot 참조}
+ */
+export const Slot = forwardRef<HTMLElement, SlotProps>(({ children, ...restProps }, forwardedRef) => {
   const childrenArray = Children.toArray(children)
   const slottable = childrenArray.find((child) => isValidElement(child) && child.type === Slottable) as ReactElement<{
     children: ReactNode
@@ -29,7 +41,16 @@ export function Slot({ children, ...restProps }: SlotProps) {
       return isValidElement(newElement) ? (newElement.props as { children: ReactNode }).children : null
     })
 
-    return isValidElement(newElement) ? cloneElement(newElement, restProps, newChildren) : null
+    return isValidElement(newElement)
+      ? cloneElement(
+          newElement,
+          {
+            ...restProps,
+            ref: composeRefs(forwardedRef, getElementRef(newElement)),
+          } as MergePropsWithRef<SlotProps>,
+          newChildren,
+        )
+      : null
   }
 
   if (Children.count(children) > 1) {
@@ -37,5 +58,25 @@ export function Slot({ children, ...restProps }: SlotProps) {
     return Children.only(null)
   }
 
-  return isValidElement(children) ? cloneElement(children, restProps) : null
+  return isValidElement(children)
+    ? cloneElement(children, {
+        ...restProps,
+        ref: composeRefs(forwardedRef, getElementRef(children)),
+      } as MergePropsWithRef<SlotProps>)
+    : null
+})
+
+Slot.displayName = "Slot"
+
+/**
+ * ReactElement의 ref를 추출하는 유틸리티 함수
+ * @param element - ReactElement
+ * @returns Ref<unknown>
+ *
+ * @see {@link https://github.com/radix-ui/primitives/blob/main/packages/react/slot/src/slot.tsx#L203 Radix UI의 getElementRef 참조}
+ *
+ * React 19에서는 element.props.ref를 사용해야하지만, 그 미만 버전에서는 element.ref로 접근합니다. 따라서 버전 차이로 인한 console.error를 방지하기 위해 두 가지를 모두 확인합니다.
+ */
+function getElementRef(element: ReactElement) {
+  return (element.props as { ref?: Ref<unknown> }).ref || (element as unknown as { ref?: Ref<unknown> }).ref
 }
