f906828ad6556a20a3b8963ac0aaf018b2e433a5
d5ng <dong5ffice@gmail.com>
2026-03-02 03:14:07 +0900

    fix: custom tailwind merge 함수 추가하여 폰트 사이즈 그룹 확장
    - tailwind-merge의 extendTailwindMerge를 사용하여 커스텀 머지 함수 구현
    - 폰트 사이즈 관련 클래스 그룹 추가로 Tailwind 테마와의 호환성 개선

--- src/lib/cn.ts
+++ src/lib/cn.ts
@@ -1,8 +1,36 @@
 import clsx from "clsx"
-import { twMerge } from "tailwind-merge"
+import { extendTailwindMerge } from "tailwind-merge"
 
 import type { ClassValue } from "clsx"
 
+// NOTE: @theme에 정의 폰트 사이즈가 누락되는 문제로 인해 Custom Tailwind Merge 함수를 사용합니다.
+const customTwMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      "font-size": [
+        {
+          text: [
+            "heading-1",
+            "heading-2",
+            "heading-3",
+            "heading-4",
+            "heading-5",
+            "body-xl",
+            "body-lg",
+            "body-md",
+            "body-sm",
+            "body-xs",
+            "action-lg",
+            "action-md",
+            "action-sm",
+            "caption-md",
+          ],
+        },
+      ],
+    },
+  },
+})
+
 export function cn(...classes: ClassValue[]) {
-  return twMerge(clsx(...classes))
+  return customTwMerge(clsx(...classes))
 }
